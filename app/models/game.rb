84dfd523c2845950a23301ef47b2c90e9cfd6454
Brian Weinreich <be.weinreich@gmail.com>
2020-08-29 11:48:54 -0400

    set initial game state

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,8 +5,14 @@ class Game < ApplicationRecord
   belongs_to :current_question, class_name: 'Question', foreign_key: 'current_question_id', optional: true
 
   before_create :generate_code
+  before_create :set_game_state
 
   def generate_code
     self.code = SecureRandom.alphanumeric[0..3].upcase
   end
+
+  def set_game_state
+    self.game_state = 'lobby'
+  end
+
 end
